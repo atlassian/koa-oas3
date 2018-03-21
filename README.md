68bbd17ec178595fb825f8551ccfde76a4bf69bc
Tong Li <tli@atlassian.com>
2018-03-21 14:57:29 +1100

    Better error handling and adds in response support

--- README.md
+++ README.md
@@ -2,31 +2,19 @@
 Request and Response validator for OpenAPI Specification 3.
 
 # Usage
+The library would expect request body to be parsed under `ctx.request.body`. You can use `koa-bodyparser` package for that.
+
 ```js
-async function oasErrors(ctx: koa.Context, next: () => Promise<any>): Promise<void> {
-  try {
-    await next();
-  } catch (e) {
-    ctx.status = 400;
-    ctx.body = {
-      code: 400,
-      message: 'Request validation failed',
-      details: {
-        where: e.where,
-        name: e.key,
-        message: e.message,
-      },
-    };
-  }
-}
+import { oas, catchValidationError } from 'koa-oas3';
 
 export async function oas3(): Promise<Router.IMiddleware> {
   return compose([
-    oasErrors,
+    catchValidationError,
     await oas<Router.IRouterContext>({
       swaggerFile: `${__dirname}/../swagger.yaml`,
       swaggerPath: '/swagger.json',
       swaggerUIPath: '/',
+      validateResponse: false
     }),
   ]);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as koa from 'koa';
 import { Config, validateConfig } from './config';
-import ChowChow from 'oas3-chow-chow';
+import ChowChow, { ChowError } from 'oas3-chow-chow';
 import { swaggerUI } from './swagger-ui';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -9,12 +9,29 @@ import * as converter from 'swagger2openapi';
 import * as compose from 'koa-compose';
 
 export class ValidationError extends Error {
-  constructor(message: string, public status: number, public validationErrors?: any[]) {
+  constructor(message: string, public status: number, public details?: any) {
     super(message);
   }
 }
 
-export async function oas<T extends koa.BaseContext>(cfg: Partial<Config>): Promise<compose.Middleware<T>> {
+export async function catchValidationError(ctx: koa.BaseContext, next: () => Promise<any>): Promise<void> {
+  try {
+    await next();
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      ctx.status = e.status;
+      ctx.body = {
+        message: e.message,
+        code: e.status,
+        details: e.details,
+      };
+    } else {
+      throw e;
+    }
+  }
+}
+
+export async function oas<T extends koa.Context>(cfg: Partial<Config>): Promise<compose.Middleware<T>> {
 
   const config = validateConfig(cfg);
   const { compiled, doc } = await compileOas(config.swaggerFile);
@@ -39,24 +56,42 @@ export async function oas<T extends koa.BaseContext>(cfg: Partial<Config>): Prom
       return await next();
     }
 
-    compiled.validateRequest(ctx.path, {
-      method: ctx.request.method,
-      header: ctx.request.header,
-      query: ctx.request.query,
-      path: ctx.params,
-      cookie: ctx.cookies,
-      body: ctx.request.body,
-    })
+    try {
+      compiled.validateRequest(ctx.path, {
+        method: ctx.request.method,
+        header: ctx.request.header,
+        query: ctx.request.query,
+        path: ctx.params,
+        cookie: ctx.cookies,
+        body: ctx.request.body,
+      })
+    } catch (e) {
+      if (e instanceof ChowError) {
+        const json = e.toJSON();
+        throw new ValidationError(json.message || 'Request validation error', json.code || 400, json);
+      } else {
+        throw new ValidationError('Request validation error', 400);
+      }
+    }
 
     await next();
 
     if (config.validateResponse) {
-      //TODO: Adds support to valiadteResponse
-      throw new Error('Validate response is under development... :)');
-      // const error = swagger.validateResponse(compiledPath, ctx.method, ctx.status, ctx.body);
-      // if (error) {
-      //   throw new ValidationError(`Response validation failed`, 500, [error]);
-      // }
+      try {
+        compiled.validateResponse(ctx.path, {
+          method: ctx.method,
+          status: ctx.status,
+          header: ctx.response.header,
+          body: ctx.body
+        })
+      } catch(e) {
+        if (e instanceof ChowError) {
+          const json = e.toJSON();
+          throw new ValidationError(json.message || 'Response validation error', 500, json);
+        } else {
+          throw new ValidationError('Response validation error', 500);
+        }
+      }
     }
   };
 }
